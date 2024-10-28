be9812684e747115b07c685a5b5d6e72b2659ad7
BaeejiH <bjihun0511@gmail.com>
2024-10-29 00:53:21 +0900

    레이아웃 및 style속성 데이터 바인딩

---
+++ src/components/ContainerBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <PostBox :인스타데이터="인스타데이터[0]"/>
+    <PostBox :인스타데이터="인스타데이터[1]"/>
+    <PostBox :인스타데이터="인스타데이터[2]"/>
+  </div>
+</template>
+
+<script>
+import PostBox from './PostBox.vue'
+
+export default {
+    components:{
+        PostBox:PostBox
+    },
+    props:{
+        인스타데이터:Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
