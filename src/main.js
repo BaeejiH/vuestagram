258c1acc0697afc7c8ec22fc5f9edc5efaa9e3c5
BaeejiH <bjihun0511@gmail.com>
2024-10-30 09:51:17 +0900

    progressive Web App

--- src/main.js
+++ src/main.js
@@ -7,4 +7,5 @@ let app =  createApp(App);
 // 글로벌한 변수 보관함, 모든 곳에서 emitter의 변수를 쓸 수 있음.
 app.config.globalProperties.emitter = emitter;
 import store from './store';
+import './registerServiceWorker'
 app.use(store).mount('#app')
